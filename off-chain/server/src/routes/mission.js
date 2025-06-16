05af247caac07fa31d7bdd7092129f9b72be1765
Loic <lhe.devpro@gmail.com>
2025-06-16 22:09:31 +0200

    API-INDEX2

--- off-chain/server/src/routes/mission.js
+++ off-chain/server/src/routes/mission.js
@@ -7,7 +7,16 @@ const fs = require('fs').promises;
 
 // Configuration CORS
 router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    const allowedOrigins = [
+        'http://localhost:3000',
+        'https://nv-mintpage-v9xa.vercel.app'
+    ];
+    
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     
